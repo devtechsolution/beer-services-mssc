4c03c117141c1f8f572dda1069abd35c85618d27
adityasrivastva <aditya5srivastva@gmail.com>
2020-10-19 20:53:44 +0530

    Initial Commit

---
+++ src/main/java/org/as/devtechsolution/beerservicesmssc/BeerServicesMsscApplication.java
@@ -0,0 +1,13 @@
+package org.as.devtechsolution.beerservicesmssc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerServicesMsscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerServicesMsscApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/as/devtechsolution/beerservicesmssc/BeerServicesMsscApplicationTests.java
@@ -0,0 +1,13 @@
+package org.as.devtechsolution.beerservicesmssc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerServicesMsscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
